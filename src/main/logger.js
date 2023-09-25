ca8e769710bedd44ec276703c3e16d019c4ca186
zaryabdev <zaryabdev404@gmail.com>
2023-09-25 19:43:27 +0500

    installed: winston

---
+++ src/main/logger.js
@@ -0,0 +1,23 @@
+const { format, createLogger, transports } = require('winston');
+
+const { combine, timestamp, label, printf } = format;
+const CATEGORY = 'winston custom format';
+
+//Using the printf format.
+const customFormat = printf(({ level, message, label, timestamp }) => {
+  return `${timestamp} [${label}] ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  level: 'debug',
+  format: combine(
+    label({ label: CATEGORY }),
+    timestamp({
+      format: 'MMM-DD-YYYY HH:mm:ss',
+    }),
+    customFormat
+  ),
+  transports: [new transports.Console()],
+});
+
+module.exports = logger;
