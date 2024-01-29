94e4211580d81785d51adebda7d0ccd4a5b8c2a3
zaryabdev <zaryabdev404@gmail.com>
2024-01-29 11:34:24 +0500

    Packaging app

--- src/renderer/modules/customers/Customers.tsx
+++ src/renderer/modules/customers/Customers.tsx
@@ -46,7 +46,7 @@ import { useRenderCount } from '@uidotdev/usehooks';
 import context from '../../AppContext';
 import { STATE, STATUS, TYPE, TYPE_COLOR_PALLETE } from '../../contants';
 import CustomersList from './CustomersList';
-// import MultiCustomersTabs from './MultiCustomersTabs';
+import MultiCustomersTabs from './MultiCustomersTabs';
 
 const { confirm } = Modal;
 type TargetKey = React.MouseEvent | React.KeyboardEvent | string;
@@ -1028,10 +1028,7 @@ function Customers({
                   style={{ marginRight: 4 }}
                   onClick={showDeleteConfirm}
                   // loading={loading}
-                  disabled={
-                    selectedRowKeys.length === 0 ||
-                    selectedCustomerType === TYPE.deleted
-                  }
+                  disabled
                 >
                   Delete
                 </Button>
@@ -1100,11 +1097,11 @@ function Customers({
           </>
         </Col>
         <Col span={appContext.toggleSideBar ? 18 : 24}>
-          {/* <MultiCustomersTabs
+          <MultiCustomersTabs
             customersList={appContext.customersList}
             getCurrentStock={getCurrentStock}
             selectedCutomersToLoad={selectedCutomersToLoad}
-          /> */}
+          />
         </Col>
       </Row>
       <Modal
